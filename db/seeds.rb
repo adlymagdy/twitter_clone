977dcc76173806a07ca07390a1d622cba5e70b0d
Adly Magdy <adly.tadros@yahoo.com>
2015-09-24 14:33:17 +0000

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name: "Adly Magdy",
              email: "adly.tadros@yahoo.com",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 99.times do |n|
   name =  Faker::Name.name
   email = "example-#{n+1}@example.com"
@@ -10,5 +12,7 @@ User.create!(name: "Adly Magdy",
   User.create!(name: name,
              email: email,
              password:              password,
-             password_confirmation: password)
+             password_confirmation: password,
+             activated: true,
+             activated_at: Time.zone.now)
 end
