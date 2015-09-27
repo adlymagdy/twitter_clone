4503a4b5743fcac9793cb5bfba25ce28cacdc860
Adly Magdy <adly.tadros@yahoo.com>
2015-09-27 15:18:23 +0000

    Add user posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,10 @@ User.create!(name: "Adly Magdy",
              activated: true,
              activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
